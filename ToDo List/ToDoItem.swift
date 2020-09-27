592aa220e90d22123250e00cabf83468608387a6
Young Hur <huryo@bc.edu>
2020-09-27 15:06:56 -0400

    Finished 5.6 - Structs

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by yhur18 on 9/27/20.
+//  Copyright © 2020 Young Hur. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name : String
+    var date: Date
+    var notes: String
+}
